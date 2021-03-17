da09968a75d00f31ea20ad7f06cd76cfe739380d
Денис <denisl_1989@mail.ru>
2021-03-17 22:30:24 +0300

    complete yandex disk client

--- yandex_disk_client.py
+++ yandex_disk_client.py
@@ -9,14 +9,17 @@ class YaUploader:
         self.host = 'https://cloud-api.yandex.net:443'
         self.publish = '/v1/disk/resources/publish'
         self.uploaded_files = '/v1/disk/resources/public'
+        self.upload_file = '/v1/disk/resources/upload'
 
     def upload(self, file_path: str):
         """Метод загруджает файл file_path на яндекс диск"""
         headers = {'Authorization': f'OAuth {self.token}'}
-        #res = requests.post()
-        response = requests.put(url=f'{self.host}{self.publish}'
-                                    f'?path={os.path.abspath(file_path)}', headers=headers, timeout=10)
-        return response.json(), response.url
+        r = requests.get(url=f'{self.host}{self.upload_file}?path={file_path}', headers=headers, timeout=10)
+        response = requests.put(url=r.json()['href'], headers=headers, timeout=10)
+        if response.status_code == 201:
+            return f'Файл {file_path} успешно загружен'
+
+
 
     def get_uploaded_files(self):
         headers = {'Authorization': f'OAuth {self.token}'}
@@ -25,6 +28,5 @@ class YaUploader:
 
 
 if __name__ == '__main__':
-    uploader = YaUploader('AgAAAAABYL-IAADLW_VMyX7tLUOjpyDLoHp2vpI')
-    print(uploader.upload('test.txt'))
-    #print(uploader.get_uploaded_files())
+    uploader = YaUploader('API_KEY')
+    print(uploader.upload('file_for_upload.txt'))
