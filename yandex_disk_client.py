b0a494075ffc4ff79a221f0a0c9ec3a978d024a6
Денис <denisl_1989@mail.ru>
2021-03-17 22:32:32 +0300

    refactoring

--- yandex_disk_client.py
+++ yandex_disk_client.py
@@ -7,8 +7,6 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
         self.host = 'https://cloud-api.yandex.net:443'
-        self.publish = '/v1/disk/resources/publish'
-        self.uploaded_files = '/v1/disk/resources/public'
         self.upload_file = '/v1/disk/resources/upload'
 
     def upload(self, file_path: str):
@@ -18,13 +16,8 @@ class YaUploader:
         response = requests.put(url=r.json()['href'], headers=headers, timeout=10)
         if response.status_code == 201:
             return f'Файл {file_path} успешно загружен'
-
-
-
-    def get_uploaded_files(self):
-        headers = {'Authorization': f'OAuth {self.token}'}
-        res = requests.get(f'{self.host}{self.uploaded_files}', headers=headers, timeout=10)
-        return res.json()
+        else:
+            return 'Ошибка'
 
 
 if __name__ == '__main__':
